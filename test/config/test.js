831704ebe8e00ac2dc9e79fe6f83786c36bbbefd
Christophe Braud <christophe@agilitation.fr>
2018-03-02 17:49:11 +0100

    Fix Linter issues

--- test/config/test.js
+++ test/config/test.js
@@ -1,30 +1,30 @@
 const host = 'http://localhost:3000';
 
 module.exports = {
-    port: 3000,
-    campsi: {
-        title: 'Test Arezzo',
-        description: 'API de test avec les pizzas Arezzo !',
-        publicURL: host,
-        mongo: {
-            'host': 'localhost',
-            'port': 27017,
-            'database': 'relationships'
-        },
-        license: {
-            name: 'MIT',
-            url: 'https://opensource.org/licenses/mit-license.php'
-        },
-        contact: {
-            name: 'Christophe Braud',
-            email: 'christophe@agilitation.fr',
-            url: 'http://agilitation.fr'
-        }
+  port: 3000,
+  campsi: {
+    title: 'Test Arezzo',
+    description: 'API de test avec les pizzas Arezzo !',
+    publicURL: host,
+    mongo: {
+      'host': 'localhost',
+      'port': 27017,
+      'database': 'relationships'
     },
-    services: {
-        swagger: {
-            title: 'Documentation',
-            ui: true
-        }
+    license: {
+      name: 'MIT',
+      url: 'https://opensource.org/licenses/mit-license.php'
+    },
+    contact: {
+      name: 'Christophe Braud',
+      email: 'christophe@agilitation.fr',
+      url: 'http://agilitation.fr'
     }
+  },
+  services: {
+    swagger: {
+      title: 'Documentation',
+      ui: true
+    }
+  }
 };
--- test/swagger.js
+++ test/swagger.js
@@ -1,11 +1,8 @@
-/**
- * Created by christophe on 16/11/17.
- */
-//During the test the env variable is set to private
+/* eslint-disable no-unused-expressions */
 process.env.NODE_CONFIG_DIR = './test/config';
 process.env.NODE_ENV = 'test';
 
-//Require the dev-dependencies
+// Require the dev-dependencies
 const debug = require('debug')('campsi:test');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
@@ -18,54 +15,56 @@ chai.use(chaiHttp);
 chai.should();
 
 const services = {
-    Swagger: require('../lib'),
+  Swagger: require('../lib')
 };
 
 describe('Swagger API', () => {
-    beforeEach((done) => {
-        //Before each test we empty the database
-        campsi = new CampsiServer(config.campsi);
-        campsi.mount('swagger', new services.Swagger(config.services.swagger));
+  beforeEach((done) => {
+    // Before each test we empty the database
+    campsi = new CampsiServer(config.campsi);
+    campsi.mount('swagger', new services.Swagger(config.services.swagger));
 
-        campsi.on('campsi/ready', () => {
-            server = campsi.listen(config.port);
-            done();
-        });
-
-        campsi.start()
-            .catch((err) => {
-                debug('Error: %s', err);
-            });
+    campsi.on('campsi/ready', () => {
+      server = campsi.listen(config.port);
+      done();
     });
 
-    afterEach((done) => {
-        server.close(() => {
-            done();
-        });
+    campsi.start()
+      .catch((err) => {
+        debug('Error: %s', err);
+      });
+  });
+
+  afterEach((done) => {
+    server.close(() => {
+      done();
     });
-    /*
+  });
+  /*
      * Test the /GET providers route
      */
-    describe('/GET swagger/swagger.json', () => {
-        it('it should return a swagger file', (done) => {
-            chai.request(campsi.app)
-                .get('/swagger/swagger.json')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.should.be.json;
-                    done();
-                });
+  describe('/GET swagger/swagger.json', () => {
+    it('it should return a swagger file', (done) => {
+      chai.request(campsi.app)
+        .get('/swagger/swagger.json')
+        .end((err, res) => {
+          if (err) debug(`received an error from chai: ${err.message}`);
+          res.should.have.status(200);
+          res.should.be.json;
+          done();
         });
     });
-    describe('/GET swagger', () => {
-        it('it should return an html file', (done) => {
-            chai.request(campsi.app)
-                .get('/swagger')
-                .end((err, res) => {
-                    res.should.have.status(200);
-                    res.should.be.html;
-                    done();
-                });
+  });
+  describe('/GET swagger', () => {
+    it('it should return an html file', (done) => {
+      chai.request(campsi.app)
+        .get('/swagger')
+        .end((err, res) => {
+          if (err) debug(`received an error from chai: ${err.message}`);
+          res.should.have.status(200);
+          res.should.be.html;
+          done();
         });
     });
+  });
 });
